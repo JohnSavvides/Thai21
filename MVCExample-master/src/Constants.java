5091b3fabdb0404b61b244df557242c1eadd3770
Marshadillo <113390418+JohnSavvides@users.noreply.github.com>
2024-05-23 10:40:14 -0700

    Add files via upload

---
+++ MVCExample-master/src/Constants.java
@@ -0,0 +1,6 @@
+public class Constants {
+    public static final int INITIAL_PILE_SIZE = 21;
+    public static final int IN_PLAY = 0;
+    public static final int P1TURN = 1;
+    public static final int P2TURN = 2;
+}
---
+++ MVCExample-master/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        NimJFrame view = new NimJFrame();
+        view.setVisible(true);
+    }
+}
